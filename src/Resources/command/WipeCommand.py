f0bdbdf26e18f13fa08340fbcb168ea1885d555b
Marcel Großmann <whatever4711@gmail.com>
2020-08-03 10:51:19 +0200

    TMUX and lab names

--- src/Resources/command/WipeCommand.py
+++ src/Resources/command/WipeCommand.py
@@ -7,8 +7,6 @@ from ..foundation.command.Command import Command
 from ..manager.ManagerProxy import ManagerProxy
 from ..setting.Setting import Setting
 from ..strings import strings, wiki_description
-from ..trdparty.libtmux.tmux import TMUX
-
 
 class WipeCommand(Command):
     __slots__ = ['parser']
